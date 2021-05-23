b84088ced223136aed24692644131d96324fe9b0
nathom <nathanthomas707@gmail.com>
2021-05-22 20:29:50 -0700

    Basic menu working

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,130 @@
 use ncurses::*;
 use std::cmp::min;
-use std::fmt::Display;
+use std::fmt;
 
 /*
  * Screen:
  * + write_line
  * + get_screen_size
- * + wait_for_key
+ * + get_key
  *
  * Menu:
  * + new(Iterable)
  * + show
  */
 
+struct Item {
+    icon: &'static str,
+    chosen_icon: &'static str,
+    chosen: bool,
+    repr: String,
+}
+
+impl Item {
+    fn new<'a>(thing: &impl fmt::Display, icon: &'static str, chosen_icon: &'static str) -> Item {
+        // let repr = format!("{} {}", icon, thing);
+        Item {
+            icon,
+            chosen_icon,
+            chosen: false,
+            repr: thing.to_string(),
+        }
+    }
+
+    fn select(&mut self) {
+        self.chosen = !self.chosen;
+    }
+
+    fn chosen(&self) -> bool {
+        self.chosen
+    }
+
+    fn icon(&self) -> &'static str {
+        if self.chosen {
+            self.chosen_icon
+        } else {
+            self.icon
+        }
+    }
+
+    fn string(&self) -> &String {
+        &self.repr
+    }
+}
+
+impl fmt::Display for Item {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", format!("{} {}", self.icon(), self.repr))
+    }
+}
+
 struct ScreenSize {
     y: i32,
     x: i32,
 }
 
-struct Screen {}
+impl ScreenSize {
+    fn new() -> ScreenSize {
+        ScreenSize { y: -1, x: -1 }
+    }
+
+    fn update(&mut self) {
+        getmaxyx(stdscr(), &mut self.y, &mut self.x);
+    }
+}
+
+struct Screen {
+    size: ScreenSize,
+}
 
 impl Screen {
     fn new() -> Screen {
-        Screen {}
+        let mut size = ScreenSize::new();
+        size.update();
+        Screen { size }
     }
 
     fn show(&self) {
+        // Allow unicode characters
         let locale_conf = LcCategory::all;
         setlocale(locale_conf, "en_US.UTF-8");
+        // Create curses screen
         initscr();
-        noecho();
-        start_color();
-        init_pair(1, COLOR_BLACK, COLOR_WHITE);
+        // Use default color background
         use_default_colors();
-        // raw();
-        // keypad(stdscr(), true);
+        // Do not show typed characters on screen
+        noecho();
+        // Allow colors
+        start_color();
+        // Color used to highlight hovered selection
+        init_pair(1, COLOR_BLACK, COLOR_WHITE);
+        // -1 means default background
+        init_pair(2, COLOR_RED, -1);
+        init_pair(3, COLOR_GREEN, -1);
+
+        // Hide cursor
         curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+
+        raw();
+        keypad(stdscr(), true);
     }
 
-    fn write_line(&self, row: i32, line: &str, selected: bool) {
-        if selected {
+    fn write_item(&self, row: i32, item: &Item, highlight: bool) {
+        mv(row, 0);
+
+        let icon_color = if item.chosen() { 3 } else { 2 };
+        attron(COLOR_PAIR(icon_color));
+        addstr(item.icon());
+        attroff(COLOR_PAIR(icon_color));
+        addch(' ' as u32);
+
+        if highlight {
             attron(COLOR_PAIR(1));
         }
-        mvaddstr(row, 0, line);
-        if selected {
+
+        addstr(item.string());
+
+        if highlight {
             attroff(COLOR_PAIR(1));
         }
     }
@@ -52,7 +133,7 @@ impl Screen {
         getmaxyx(stdscr(), &mut size.y, &mut size.x);
     }
 
-    fn wait_for_key(&self, key: i32) {
+    fn _wait_for_key(&self, key: i32) {
         loop {
             if getch() == key {
                 break;
@@ -67,79 +148,142 @@ impl Screen {
     fn refresh(&self) {
         refresh();
     }
-}
 
-impl Drop for Screen {
-    fn drop(&mut self) {
+    fn erase(&self) {
+        erase();
+    }
+
+    fn max_y(&mut self) -> usize {
+        self.size.update();
+        self.size.y as usize
+    }
+
+    fn _max_x(&mut self) -> usize {
+        self.size.update();
+        self.size.x as usize
+    }
+
+    fn end(&self) {
         endwin();
     }
 }
 
-pub struct Menu<'a> {
-    list: Vec<String>,
-    screen: Screen,
-    item_icon: &'a str,
-    chosen_item_icon: &'a str,
-
-    down_keys: Vec<i32>,
-    up_keys: Vec<i32>,
-    select_keys: Vec<i32>,
-    multiselect_keys: Vec<i32>,
-
-    curr_row: usize,
-    first_idx: usize,
-    last_idx: usize,
+enum MenuReturnCode {
+    Done,
+    Pass,
 }
 
-impl Menu<'_> {
-    pub fn new(list: Vec<String>) -> Menu<'static> {
+struct MenuState {
+    row: usize,
+    range: (usize, usize),
+    items: Vec<Item>,
+}
+
+struct Keys {
+    down: Vec<i32>,
+    up: Vec<i32>,
+    select: Vec<i32>,
+    multiselect: Vec<i32>,
+}
+
+struct MenuConfig {
+    multiselect: bool,
+}
+
+pub struct Menu {
+    list: Vec<String>,
+    // list: Vec<Item<'a>>,
+    screen: Screen,
+    item_icon: &'static str,
+    chosen_item_icon: &'static str,
+    selection: Vec<usize>,
+    keys: Keys,
+
+    state: MenuState,
+    config: MenuConfig,
+}
+
+use MenuReturnCode::{Done, Pass};
+
+type RetCode = MenuReturnCode;
+impl Menu {
+    pub fn new(list: Vec<String>) -> Menu {
         let screen = Screen::new();
+
+        let item_icon: &'static str = "❯";
+        let chosen_item_icon: &'static str = "~";
+
         Menu {
             list,
             screen,
-            item_icon: "❯",
-            chosen_item_icon: "~",
+            item_icon: &item_icon,
+            chosen_item_icon: &chosen_item_icon,
+            selection: Vec::new(),
 
-            down_keys: vec![KEY_DOWN, 'j' as i32],
-            up_keys: vec![KEY_UP, 'k' as i32],
-            select_keys: vec![10],
-            multiselect_keys: vec![20],
+            keys: Keys {
+                down: vec![KEY_DOWN, 'j' as i32],
+                up: vec![KEY_UP, 'k' as i32],
+                select: vec![10],
+                multiselect: vec![32],
+            },
 
-            curr_row: 0,
-            first_idx: 0,
-            last_idx: 0,
+            state: MenuState {
+                row: 0,
+                range: (0, 0),
+                items: Vec::new(),
+            },
+
+            config: MenuConfig { multiselect: true },
         }
     }
 
-    pub fn show(&mut self) {
+    pub fn show(&mut self) -> Vec<usize> {
         let mut size = ScreenSize { y: -1, x: -1 };
         self.screen.show();
         self.screen.get_size(&mut size);
-        self.last_idx = min(self.list.len(), size.y as usize);
+        self.state.range.1 = min(self.list.len(), size.y as usize);
 
         self.refresh();
 
         loop {
             match self.screen.get_key() {
                 27 | 113 => break, // ESC or q
-                val => {
-                    self.handle_key(val);
 
-                    endwin();
-                    self.refresh();
+                val => {
+                    self.screen.erase();
+                    match self.handle_key(val) {
+                        Pass => self.refresh(),
+                        Done => break,
+                    }
                 }
             }
         }
+
+        self.screen.end();
+        self.finish()
+    }
+
+    fn finish(&self) -> Vec<usize> {
+        self.selection.clone()
     }
 
     fn refresh(&mut self) {
         let mut size = ScreenSize { y: -1, x: -1 };
         self.screen.get_size(&mut size);
 
-        for (row, item) in self.list[self.first_idx..self.last_idx].iter().enumerate() {
-            let item = self.get_repr(item);
+        let slice = self.state.range.0..self.state.range.1;
+        for (row, i) in slice.enumerate() {
+            // TODO: compare sizes instead of retrieving value
+            if let None = self.state.items.get(i) {
+                self.state.items.push(Item::new(
+                    &self.list[i],
+                    self.item_icon,
+                    self.chosen_item_icon,
+                ));
+            }
+            let item = &self.state.items[i];
             self.screen
-                .write_line(row as i32, &item, self.curr_row == row);
+                .write_item(row as i32, item, self.state.row == row);
             if row > size.y as usize {
                 break;
             }
@@ -148,51 +292,82 @@ impl Menu<'_> {
         self.screen.refresh();
     }
 
-    fn handle_key(&mut self, val: i32) {
-        if self.down_keys.contains(&val) {
-            // self.scroll(-1);
-            self.move_selection(1);
-        } else if self.up_keys.contains(&val) {
-            // self.scroll(1);
-            self.move_selection(-1);
-        } else if self.select_keys.contains(&val) {
-            self.select_item();
-        } else if self.multiselect_keys.contains(&val) {
-            self.multiselect_item();
+    fn handle_key(&mut self, val: i32) -> RetCode {
+        if self.keys.down.contains(&val) {
+            self.move_selection(1)
+        } else if self.keys.up.contains(&val) {
+            self.move_selection(-1)
+        } else if self.config.multiselect && self.keys.multiselect.contains(&val) {
+            self.multiselect_item()
+        } else if self.keys.select.contains(&val) {
+            self.select_item()
+        } else {
+            Pass
         }
-        // println!(
-        //     "{}, {}, {:?}, {}",
-        //     self.first_idx, self.last_idx, self.list, self.curr_row
-        // );
     }
+
+    fn select_item(&mut self) -> RetCode {
+        let curr_item_idx = self.state.range.0 + self.state.row;
+        match self.selection.last() {
+            Some(&num) if num == curr_item_idx => return Done,
+            _ => (),
+        }
+        self.state.items[curr_item_idx].select();
+        self.selection.push(curr_item_idx);
+        Done
+    }
+
+    fn multiselect_item(&mut self) -> RetCode {
+        let curr_item_idx = self.state.range.0 + self.state.row;
+        self.state.items[curr_item_idx].select();
+        self.selection.push(curr_item_idx);
+        Pass
+    }
+
+    // fn multiselect_item(&self) {}
 
     fn scroll(&mut self, amount: i32) {
-        self.first_idx = ((self.first_idx as i32) + amount) as usize;
-        self.last_idx = ((self.last_idx as i32) + amount) as usize;
+        self.state.range.0 = ((self.state.range.0 as i32) + amount) as usize;
+        self.state.range.1 = ((self.state.range.1 as i32) + amount) as usize;
     }
 
-    fn move_selection(&mut self, amount: i32) {
-        let temp = self.curr_row;
-        self.curr_row = ((self.curr_row as i32) + amount) as usize;
-        if self.curr_row >= self.list.len() {
-            self.curr_row = temp;
+    fn move_selection(&mut self, amount: i32) -> RetCode {
+        let max_y = self.screen.max_y() as f64;
+        if self.state.row > (max_y * 0.67) as usize && self.state.range.1 < self.list.len() {
+            self.scroll(1);
+            self.state.row -= 1;
+        } else if self.state.row < (max_y * 0.33) as usize && self.state.range.0 > 0 {
+            self.scroll(-1);
+            self.state.row += 1;
         }
+
+        let next_row = ((self.state.row as i32) + amount) as usize;
+        if next_row < min(self.list.len(), self.screen.max_y()) {
+            self.state.row = next_row;
+        }
+
+        Pass
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn def_item_formatting() {
+        let icon = String::from("xx");
+        let chosen = String::from("--");
+        let mut item = Item::new("test string", &icon, &chosen);
+        assert_eq!(item.to_string(), "xx test string");
     }
 
-    fn select_item(&mut self) {
-        let selection = self.curr_row + self.first_idx;
-        self.list[selection] = self.list[selection].replace(self.item_icon, self.chosen_item_icon);
-    }
-
-    fn multiselect_item(&mut self) {
-        let selection = self.curr_row + self.first_idx;
-        self.list[selection] = self.list[selection].replace(self.item_icon, self.chosen_item_icon);
-    }
-
-    pub fn get_repr(&self, item: impl Display) -> String {
-        let mut line = String::from(self.item_icon);
-        line.push_str(" ");
-        line.push_str(&item.to_string());
-        line
+    #[test]
+    fn chosen_item_fomatting() {
+        let icon = String::from("xx");
+        let chosen = String::from("--");
+        let mut item = Item::new("test string", &icon, &chosen);
+        item.select();
+        assert_eq!(item.to_string(), "-- test string");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
 mod lib;
 
-// #[cfg(feature = "wide")]
 fn main() {
-    let list: Vec<String> = vec![
-        "there".to_string(),
-        "are".to_string(),
-        "a".to_string(),
-        "few".to_string(),
-        "thing".to_string(),
-        "s".to_string(),
-        "tha".to_string(),
-        "ti".to_string(),
-        "i".to_string(),
-        "thought".to_string(),
-        "would".to_string(),
-        "be".to_string(),
-        "good".to_string(),
-    ];
+    let list: Vec<&str> = "there is some text that could show up in the menu \
+                           if the person that was making the menu chose to \
+                               add this text it would be nice"
+        .split(" ")
+        .collect();
 
+    let list = list.iter().map(|s| s.to_string()).collect();
     let mut menu = lib::Menu::new(list);
-    menu.show();
+    let choice = menu.show();
+    println!("Chose {:?}", choice);
 }
